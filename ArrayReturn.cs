feb528e5a58928d621690d3c5479e26fb9f0707e
jwtay <jwtay@JTS_M15R7>
2024-01-17 21:58:21 -0700

    Add project files.

---
+++ ArrayReturn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    internal class ArrayReturn
+    {
+        public static Array CalculateArray(int numberOfRolls)
+        {
+            int[] diceArray = new int[11];
+            Random rnd = new Random();
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int Dice1 = rnd.Next(1, 7);
+                int Dice2 = rnd.Next(1, 7);
+                int diceResult = Dice1 + Dice2;
+
+                diceArray[diceResult - 2]++;
+            }
+            return diceArray;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Mission2Assignment;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numOfRolls = 0;
+        int[] diceArray;
+        float numRollsTwo = 0;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator! \n");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        numOfRolls = int.Parse(System.Console.ReadLine());
+        numRollsTwo = numOfRolls;
+
+        diceArray = (int[])ArrayReturn.CalculateArray(numOfRolls);
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS\n");
+        System.Console.WriteLine("Each '*' represents 1% of the total number of rolls");
+        System.Console.WriteLine("Total number of rolls = " +  numOfRolls + "\n");
+
+        for (int i = 0; i < diceArray.Length; i++) 
+        {
+            int numAsterik = (((int)((diceArray[i] / numRollsTwo) * 100)));
+            System.Console.WriteLine((i + 2) + ": " + (new string('*', numAsterik)));
+        }
+
+        System.Console.WriteLine("\n Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
